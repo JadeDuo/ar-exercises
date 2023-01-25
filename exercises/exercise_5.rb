90ce82b4739a0aecaaeb47432f6659680034327d
Jade <jade.duong@telus.com>
2023-01-24 22:41:42 -0800

    add ex 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@revenue = Store.sum(:annual_revenue)
+
+puts "revenue total sum is #{@revenue}"
+
+@top_stores = Store.where(annual_revenue: 1000000..).count
+
+puts "#{@top_stores} stores have over 1 mil revenue"
